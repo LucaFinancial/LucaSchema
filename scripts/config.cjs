1ae1dc05b729779ed6e28088cd7ef64d13a343d9
Johnathan <jwaspin@users.noreply.github.com>
2024-04-28 19:43:14 -0600

    Fix/exports (#11)
    * chore: Update file paths and copy functions for ESM build
    
    * chore: Update package.json version to 1.0.7-beta.2

--- scripts/config.cjs
+++ scripts/config.cjs
@@ -1,21 +1,23 @@
-const path = require("path");
+const path = require('path');
 
-const srcPath = path.join(__dirname, "..", "src");
-const examplesSrc = path.join(srcPath, "examples");
-const indexSrc = path.join(srcPath, "index.js");
-const schemasSrc = path.join(srcPath, "schemas");
-const validatorsSrc = path.join(srcPath, "validators.js");
+const srcPath = path.join(__dirname, '..', 'src');
+const constantsSrc = path.join(srcPath, 'constants.js');
+const examplesSrc = path.join(srcPath, 'examples');
+const indexSrc = path.join(srcPath, 'index.js');
+const schemasSrc = path.join(srcPath, 'schemas');
+const validatorSrc = path.join(srcPath, 'lucaValidator.js');
 
-const distPath = path.join(__dirname, "..", "dist");
+const distPath = path.join(__dirname, '..', 'dist');
 
-const cjsPath = path.join(distPath, "cjs");
-const cjsExamplesDst = path.join(cjsPath, "examples");
-const cjsSchemasDst = path.join(cjsPath, "schemas");
+const cjsPath = path.join(distPath, 'cjs');
+const cjsExamplesDst = path.join(cjsPath, 'examples');
+const cjsSchemasDst = path.join(cjsPath, 'schemas');
 
-const esmPath = path.join(distPath, "esm");
-const esmIndexDst = path.join(esmPath, "index.js");
-const esmSchemasDst = path.join(esmPath, "schemas");
-const esmValidatorsDst = path.join(esmPath, "validators.js");
+const esmPath = path.join(distPath, 'esm');
+const esmConstantsDst = path.join(esmPath, 'constants.js');
+const esmIndexDst = path.join(esmPath, 'index.js');
+const esmSchemasDst = path.join(esmPath, 'schemas');
+const esmValidatorDst = path.join(esmPath, 'lucaValidator.js');
 
 module.exports = {
   distPath,
@@ -23,12 +25,14 @@ module.exports = {
   esmPath,
   cjsExamplesDst,
   cjsSchemasDst,
+  esmConstantsDst,
   esmIndexDst,
-  esmValidatorsDst,
+  esmValidatorDst,
   esmSchemasDst,
   srcPath,
+  constantsSrc,
   examplesSrc,
   indexSrc,
   schemasSrc,
-  validatorsSrc,
+  validatorSrc
 };
